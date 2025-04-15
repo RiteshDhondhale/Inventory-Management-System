de89493c1b8e85c19b1bf8b2f0de1931b72bcb4c
RiteshDhondhale <163043089+RiteshDhondhale@users.noreply.github.com>
2025-04-15 11:20:07 +0530

    Delete server directory

--- server/routes/SaleRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createSale, getAllSales } from "../controllers/SaleController.js";
-
-const saleRoutes = Router();
-
-saleRoutes.post("/new-sale", createSale);
-saleRoutes.get("/get-all-sales", getAllSales);
-
-export default saleRoutes;
